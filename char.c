ed69549efd119b7ab668856d4fe04257e7fd44d3
sudhachinnasamy <30309481+sudhachinnasamy@users.noreply.github.com>
2017-07-20 13:59:27 +0530

    char.c

---
+++ char.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+#include <string.h>
+void main()
+{
+   char str[100];
+   int i = 0, l = 0, f = 1;
+   clrscr();
+   puts("Enter any string\n");
+   gets(str);
+   for(i = 0; str[i] !='\0'; i++)
+   {
+      l = l + 1;
+   }
+   printf("The number of characters in the string are %d\n", l);
+  
+}
