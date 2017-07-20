69f6cf4fbc356ded61cd835d1d8df278111a7669
sudhachinnasamy <30309481+sudhachinnasamy@users.noreply.github.com>
2017-07-20 14:18:33 +0530

    dif.c

---
+++ dif.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+int ninja,opponent,difference;
+scanf("%d",&ninja);
+scanf("\n%d",&opponent);
+if(ninja<pow(2,32)&&opponent<pow(2,32));
+    difference=ninja-opponent;
+    printf("%d",difference);
+ }
